1dfb0cf22ad9cc86e340ec9135080b292d857bb3
shravan20 <mrshravankumarb@gmail.com>
2025-07-17 03:36:46 +0530

    feat: fix deployment build vs local build base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/traffik/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/traffik/' : '/',
   server: {
     host: "::",
     port: 8080,
